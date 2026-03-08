625ed5bac13bfa9d45a7966ae94da7de2a2bc5e1
vr <vr@paykeeper.ru>
2026-03-08 18:12:15 +0200

    Added task Вычислить факториал2.0

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,9 +1,7 @@
 function factorial(n) {
-  if (n < 0) {
-    return null;
-  } else if (n === 0 || n === 1) {
-    return 1;
-  } else {
-    return n * factorial(n - 1);
+let i = 1;
+  while(n > 1) {
+    i *= n--;
   }
+return i;
 }
