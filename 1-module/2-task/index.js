ae0ac4c26c8e28d8c16ff87cdc36653997c7242c
vr <vr@paykeeper.ru>
2026-03-08 19:50:47 +0200

    Refactor isValid function for improved readability and correctness

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,12 +10,9 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  if (name !== '' && name.length >= 4 && !name.includes(" ")) {
-    return true;
-  } else {
-    return false;
-  }
+  return name !== null && name.length >= 4 && !name.includes(' ');
 }
+
 
 function sayHello() {
   let userName = prompt('Введите ваше имя');
